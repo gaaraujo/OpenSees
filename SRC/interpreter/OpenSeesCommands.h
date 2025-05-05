e77112c487711c0ab6f2f497805588fc7b5ee7e2
gaaraujo <garaujor@stanford.edu>
2025-05-05 02:37:22 -0700

    added AmgX to the list of OPS_ functions

--- SRC/interpreter/OpenSeesCommands.h
+++ SRC/interpreter/OpenSeesCommands.h
@@ -501,6 +501,7 @@ void* OPS_PFEMSolver_Laplace();
 void* OPS_PFEMSolver_LumpM();
 void* OPS_SymSparseLinSolver();
 void* OPS_FullGenLinLapackSolver();
+void* OPS_AmgXGenLinSolver();
 
 void* OPS_PlainNumberer();
 
